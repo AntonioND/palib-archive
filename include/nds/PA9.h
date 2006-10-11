bbb5a8a1049173fe50c5bcd9d1a99abca3946157
elhobito <elhobito@7b02e84e-43ec-46ad-a041-a6fe5633c533>
2006-10-11 15:01:38 +0000

    git-svn-id: svn://localhost/tmp/palibds/trunk@96 7b02e84e-43ec-46ad-a041-a6fe5633c533

--- include/nds/PA9.h
+++ include/nds/PA9.h
@@ -14,7 +14,7 @@ extern "C" {
 
 #include <stdarg.h> // Pour le système de text
 #include <string.h>
-
+#include "arm9/PA_IO.h"
 #include "gbfs.h"
 #include "gba-jpeg-decode.h"
 #include "Sound9.h"
@@ -56,7 +56,6 @@ extern "C" {
 
 #include "arm9/PA_TileDual.h"
 
-#include "arm9/PA_FAT.h"  // fat driver by chrishm
 
 #include "arm9/PA_Keyboard.h"
 
